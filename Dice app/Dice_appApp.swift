bd58043753b7934a367b8eaa1cbc737d56925d1e
Gozde <gozde.a.997@gmail.com>
2024-09-02 11:01:49 +0300

    Initial Commit

---
+++ Dice app/Dice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_appApp.swift
+//  Dice app
+//
+//  Created by Gözde Aydin on 2.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
