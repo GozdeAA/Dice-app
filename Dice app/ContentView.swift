1a08b34d27149fdb5e97a4e7db19e37e6524e948
Gozde <gozde.a.997@gmail.com>
2024-09-02 11:48:57 +0300

    initial

--- Dice app/ContentView.swift
+++ Dice app/ContentView.swift
@@ -8,17 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDice = 1
+    @State var rightDice = 1
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/ .fill/*@END_MENU_TOKEN@*/)
+
+            VStack(content: {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(number: leftDice)
+                    DiceView(number: rightDice)
+
+                }.frame(width: 350)
+                Spacer()
+                Button(action: oooy) {
+                    Text("oy")
+                        .font(.system(size: 50))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding()
+                }.background(.red)
+                    .clipShape(.buttonBorder)
+            })
         }
-        .padding()
+    }
+
+    func oooy() {
+        leftDice = Int.random(in: 1 ... 6)
+        rightDice = Int.random(in: 1 ... 6)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DiceView: View {
+    let number: Int
+    var body: some View {
+        Image("dice\(number)").resizable()
+            .aspectRatio(1, contentMode: .fit).padding()
+    }
 }
